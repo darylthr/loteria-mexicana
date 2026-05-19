c5805d35c46fb34fde9d7a54e3e29f952057a770
darylthr <darylthr@gmail.com>
2026-05-19 10:14:31 -0500

    feat: configure React Router with Home, Lobby, and Game routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Lobby from './pages/Lobby'
+import Game from './pages/Game'
+
 export default function App() {
-  return <div>Lotería</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lobby/:roomId" element={<Lobby />} />
+        <Route path="/game/:roomId" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
